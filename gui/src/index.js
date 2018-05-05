966a281a9498c8dd710f7785ef6f9546553ff418
Huguinho <huguinho27@users.noreply.github.com>
2018-05-05 22:56:34 +0100

    Added list of trouble tickets.

--- gui/src/index.js
+++ gui/src/index.js
@@ -16,7 +16,7 @@ app.on('ready', function(){
   mainWindow = new BrowserWindow({});
   // Load html in window
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'mainWindow.html'),
     protocol: 'file:',
     slashes:true
   }));
