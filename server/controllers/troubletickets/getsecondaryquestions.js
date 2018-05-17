85505d854f487534eac272d6992a5e1c2befb97d
Nuno Neto <nuno_mgl@hotmail.com>
2018-05-17 18:21:40 +0100

    get secondary questions done

---
+++ server/controllers/troubletickets/getsecondaryquestions.js
@@ -0,0 +1,32 @@
+const mongo = require('../../models/db');
+
+module.exports =
+{
+    post: (req, res) =>
+    {
+        const id = req.body.id;
+
+        if(id === undefined || id === '')
+            res.send({'error':1, 'message':'Invalid given id'});
+
+        mongo.getSecondaryQuestionByTroubleTicketId(req.body, (err, result) =>
+        {
+           if(err !== null)
+               res.send({'error':1, 'message':'Failed to retrieve secondary Tickets'});
+           else
+           {
+               result.forEach((sq) =>
+               {
+                  sq.id = sq._id;
+                  delete sq._id;
+               });
+
+               res.send({
+                   'error':0,
+                   'message':'OK',
+                   'secondaryQuestions': result
+               });
+           }
+        });
+    }
+};
--- server/controllers/troubletickets/index.js
+++ server/controllers/troubletickets/index.js
@@ -1,8 +1,10 @@
 const routes = require('express').Router();
 const add = require('./add');
 const get = require('./get');
+const getsecondaryquestions = require('./getsecondaryquestions');
 
 routes.route('/add').post(add.post);
 routes.route('/get').post(get.post);
+routes.route('/getsecondaryquestions').post(getsecondaryquestions.post);
 
 module.exports = routes;
